cde30e80f1b687692ef4ca221bc1aba311b54444
Navid <navid8020@gmail.com>
2023-12-11 12:07:39 +0330

    HW4-Synchronization-Q2

---
+++ Assignments/HW4/Questions/Q2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+// semaphore declarations
+......
+
+void* p1(void* arg){
+
+}
+
+void* p2(void* arg){
+
+}
+
+
+
+int main(){
+    // semaphore initializations
+    ......
+
+    // thread creations
+
+
+    return 0;
+}
